1eb8493ed3bcec95a81f5bab271ba0f9a9acfdd4
Steven Rosario <strosariogarcia@gmail.com>
2022-09-12 10:31:35 -0400

    commit tree 7062923e5305db7b2900990b6c168997c7bb16e8

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+
+// Your code here
+
+class Cat {
+    constructor(name, sex){
+        this.name = name;
+        this.sex = sex;
+    }
+
+    speak(name){
+        return `${this.name} says meow!`
+    }
+}
+
+class Dog {
+    constructor(name, sex){
+        this.name = name;
+        this.sex = sex;
+    }
+
+    speak(name){
+        return `${this.name} says woof!`
+    }
+}
+
+class Bird {
+    constructor(name, sex){
+        this.name = name;
+        this.sex = sex;
+    }
+
+    speak(name){
+        let talk = this.sex == 'male' ? `It's me! ${this.name}, the parrot!` : `${this.name} says squawk!`
+        return talk
+    }
+}
